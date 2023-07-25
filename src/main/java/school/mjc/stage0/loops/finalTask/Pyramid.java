148207b7cddf4a4a3f11d15664a21cd5d5d02bbe
polina <polinatuzankina@gmail.com>
2023-07-25 18:01:44 +0600

    module 4.2 - final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,26 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int spaces = cathetusLength;
+        int startNumberInRow = 1;
 
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 1; j < spaces; j++) {
+                System.out.print(' ');
+            }
+
+            for (int j = startNumberInRow; j > 0; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= startNumberInRow; j++) {
+                System.out.print(j);
+            }
+
+            spaces--;
+            startNumberInRow++;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
